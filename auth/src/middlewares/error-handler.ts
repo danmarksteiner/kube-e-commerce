a55772bc64607e9867f59af9ba96ec8bb7dce12a
danmarksteiner <marksteiner.dan@gmail.com>
2020-07-20 14:01:54 +0100

    Adds custom errors to the error handler

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,12 +1,22 @@
 import { Request, Response, NextFunction } from 'express';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
 
+// Error handler middleware
+// Identifies the type of error set by the extended subclasses of Error potentionally thrown in the signup process
 export const errorHandler = (
   err: Error,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  console.log('Something went wrong', err);
+  if (err instanceof RequestValidationError) {
+    console.log('Handling this as a request validation error');
+  }
+
+  if (err instanceof DatabaseConnectionError) {
+    console.log('Handling this as a db connection error');
+  }
 
   res.status(400).send({
     message: err.message,
