cb95f8935eb9bea0e05ed2536bf9b239e645aaf1
danmarksteiner <marksteiner.dan@gmail.com>
2020-07-21 12:22:43 +0100

    Adds not found error handling into the index.ts express file

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,14 +6,21 @@ import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
 import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.use(json());
 
+// Setup our API routes
 app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+// If not a designated route throw a new not found 404 error
+app.all('*', () => {
+  throw new NotFoundError();
+});
 
 app.use(errorHandler);
 
