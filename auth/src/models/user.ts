6d14d66d5354c9b26ab941c3bb15372c526215b9
danmarksteiner <marksteiner.dan@gmail.com>
2020-07-22 11:23:27 +0100

    Adds type checking code to the user model

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,6 +1,12 @@
 // Mongoose User model to govern access to the user collection and determine how we add to, access or modify the data stored within it
 import mongoose from 'mongoose';
 
+// An interface that describes the properties that are required to create a new user
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
 // Schema tells mongoose about all the properties a user will have
 const userSchema = new mongoose.Schema({
   email: {
@@ -15,5 +21,12 @@ const userSchema = new mongoose.Schema({
 
 // Feed schema into mongoose and create a model for accessing data
 const User = mongoose.model('User', userSchema);
+
+// We call this instead of 'new User' anytime we want to create a user
+// Used to help TS and Mongoose work together and check correct types
+// Makes TS aware of the properties needed to create a User
+const buildUser = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
 
 export { User };
