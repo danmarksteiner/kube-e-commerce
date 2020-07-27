ea8b60f8be74eaca1adc9e1939f79cee882dbb8a
danmarksteiner <marksteiner.dan@gmail.com>
2020-07-27 10:32:06 +0100

    Signin route boilerplate

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,9 +1,32 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+
+import { RequestValidationError } from '../errors/request-validation-error';
 
 const router = express.Router();
 
-router.post('/api/users/signin', (req, res) => {
-  res.send('Hi there!');
-});
+router.post(
+  '/api/users/signin',
+  [
+    // Validate the incoming request
+    // Check for a valid email
+    body('email').isEmail().withMessage('Email must be valid'),
+    // Check password
+    body('password')
+      // Trim spaces
+      .trim()
+      // Check a password is provided
+      .notEmpty()
+      .withMessage('You must supply a password'),
+  ],
+  (req: Request, res: Response) => {
+    // Check if anything went wrong with the validation check
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      // Throw new validation error and pass in the errors array
+      throw new RequestValidationError(errors.array());
+    }
+  }
+);
 
 export { router as signinRouter };
