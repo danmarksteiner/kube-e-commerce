f8e7c832b9ec36efaa8591c045687a249dc53fad
danmarksteiner <marksteiner.dan@gmail.com>
2020-07-27 13:42:46 +0100

    Adds base require-auth middleware and not authroized custom error

--- auth/src/errors/not-authorized-error.ts
+++ auth/src/errors/not-authorized-error.ts
@@ -1,10 +1,12 @@
 import { CustomError } from './custom-error';
 
+// Subclass extends custom error to ensure we include the right variables and methods in our error response
 export class NotAuthorizedError extends CustomError {
   statusCode = 401;
 
   constructor() {
     super('Not authorized');
+    // Only because we are extending a built in class
     Object.setPrototypeOf(this, NotAuthorizedError.prototype);
   }
 
