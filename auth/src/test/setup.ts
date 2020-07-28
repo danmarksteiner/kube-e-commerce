b0c659813f6c5eb005a8d2de51c53f6cabe55144
danmarksteiner <marksteiner.dan@gmail.com>
2020-07-28 14:07:54 +0100

    Adds boilerplate for tests setup

---
+++ auth/src/test/setup.ts
@@ -0,0 +1,35 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import { app } from '../app';
+
+// Define mongo ahead of time so we can access it in multiple hook functions
+let mongo: any;
+
+// Create new instance of mongo in memory
+// Stops multiple services from potentially reaching out to the same copy of mongo
+// Also grants direct access to the database for easier testing
+// Hook function will run before all tests
+beforeAll(async () => {
+  mongo = new MongoMemoryServer();
+  const mongoUri = await mongo.getUri();
+
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+});
+
+// Hook function will run before each tests
+beforeEach(async () => {
+  // Delete all data in between each test
+  const collections = await mongoose.connection.db.collections();
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+// Hook function will run after all tests to shut down in memory mongoDb
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
