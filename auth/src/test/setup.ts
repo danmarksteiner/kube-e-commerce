a16eb3683b3184efe3b7c3b562e703f364d625df
danmarksteiner <marksteiner.dan@gmail.com>
2020-07-29 11:27:48 +0100

    Adds authenticated request helper and adds this to currentuser tests

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,6 +1,16 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
+import request from 'supertest';
 import { app } from '../app';
+
+// For TS declare the signin helper onto the global object and define it's return value
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signin(): Promise<string[]>;
+    }
+  }
+}
 
 // Define mongo ahead of time so we can access it in multiple hook functions
 let mongo: any;
@@ -37,3 +47,21 @@ afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 });
+
+// Signin helper function to allow authenticated requests with a cookie easily
+global.signin = async () => {
+  const email = 'test@test.com';
+  const password = 'password';
+
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email,
+      password,
+    })
+    .expect(201);
+
+  const cookie = response.get('Set-Cookie');
+
+  return cookie;
+};
