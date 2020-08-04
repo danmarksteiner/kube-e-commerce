730654930f0052a0ff9ebdf847509cb4966b8cea
danmarksteiner <marksteiner.dan@gmail.com>
2020-08-04 10:51:46 +0100

    Add email and password hooks and onSubmit function to signup form

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.css';
 // Custom App Component
 // Acts like a wrapper for the app so we can include global bootstrap css file
+// Ref - https://github.com/vercel/next.js/blob/canary/errors/css-global.md
 export default ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
 };
