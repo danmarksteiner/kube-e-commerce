27d6ece01ebfb7a386137def133fa2fe419a4f4e
danmarksteiner <marksteiner.dan@gmail.com>
2020-08-04 11:10:01 +0100

    Adds basic validation logic

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,13 +1,25 @@
 import { useState } from 'react';
+import axios from 'axios';
 
 export default () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState([]);
 
-  const onSubmit = event => {
+  const onSubmit = async event => {
     event.preventDefault();
 
-    console.log(email, password);
+    try {
+      const response = await axios.post('/api/users/signup', {
+        email,
+        password,
+      });
+
+      console.log(response.data);
+    } catch (err) {
+      // Catch any errors from response and set them on the errors array state
+      setErrors(err.response.data.errors);
+    }
   };
 
   return (
@@ -30,6 +42,16 @@ export default () => {
           className="form-control"
         />
       </div>
+      {errors.length > 0 && (
+        <div className="alert alert-danger">
+          <h4>Oops</h4>
+          <ul className="my-0">
+            {errors.map(err => (
+              <li key={err.message}>{err.message}</li>
+            ))}
+          </ul>
+        </div>
+      )}
       <button className="btn btn-primary">Sign Up</button>
     </form>
   );
